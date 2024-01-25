6582bdb4d164d7d21375447ee318e390724c05a4
go5raeismine <157711096+go5raeismine@users.noreply.github.com>
2024-01-25 22:35:59 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Total days in Year"
+    <<"\n365"
+    <<"\nCircumference rate"
+    <<"\n3.1415926535";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
