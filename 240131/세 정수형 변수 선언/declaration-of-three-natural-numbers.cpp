93303a98cfcc97be015e32d0c4af6ec1a8eec10b
go5raeismine <157711096+go5raeismine@users.noreply.github.com>
2024-01-31 14:54:59 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int a = 7;
+    int b = 23;
+    int c = a+b;
+
+    cout << a << " + " << b << " = " << c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
