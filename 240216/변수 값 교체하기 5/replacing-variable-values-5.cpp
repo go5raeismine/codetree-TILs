153f8a8407d77e49b3d2418c2c2933f8f9ad61fa
go5raeismine <157711096+go5raeismine@users.noreply.github.com>
2024-02-16 02:22:03 +0900

    Title: 변수 값 교체하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 2;
+    int b = 6;
+
+    a=b;
+
+    cout<<a<<endl;
+    cout<<b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
