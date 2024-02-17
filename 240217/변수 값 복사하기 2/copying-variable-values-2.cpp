39741c89ceead7f6e2b69762070da7eaa74854f5
go5raeismine <157711096+go5raeismine@users.noreply.github.com>
2024-02-17 14:53:05 +0900

    Title: 변수 값 복사하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240217/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    a=b=c;
+
+    cout<<a<<" "<<b<<" "<<c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
