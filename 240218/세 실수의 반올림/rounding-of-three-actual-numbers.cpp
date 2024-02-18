cbec8d1a61d1e06c645737ebccebab4c7ad8540e
go5raeismine <157711096+go5raeismine@users.noreply.github.com>
2024-02-18 13:40:17 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    double b;
+    double c;
+
+    cin>>a>>b>>c;
+
+    cout<<fixed;
+    cout.precision(3);
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
